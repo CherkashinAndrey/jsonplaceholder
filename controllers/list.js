0ea2ed33bc0bee16c90e5e5ac8a92f72c1081a89
Andrey Cherkashin <tosmsandreyka@gmail.com>
2016-01-28 00:57:06 +0200

    two commit

--- controllers/list.js
+++ controllers/list.js
@@ -1,17 +1,13 @@
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
+var objUrl = {
+                section: 'users',
+                el: 'userId',
+                el_id: '',
+             }
 
-app.constant('URL', 'http://jsonplaceholder.typicode.com/users');
-
-URLs = {
-    ROOT: '',
-    POSTS:'',
-    COMMENTS:'',
-    ALBUMS:'',
-    PHOTOS:'',
-    TODOS:'',
-    USERS:'',
-}
+app.constant('URL', 'http://jsonplaceholder.typicode.com/'+ objUrl.section+ "?" + objUrl.el + "=" + objUrl.el_id);
+//url: ROOT_URL + obj.section+ "?" + obj.el + "=" + obj.el_id
 
 app.factory('getDataService', function($http, URL) {
     return {
@@ -34,12 +30,75 @@ app.controller('Controller', function($scope, getDataService) {
     });
 
     $scope.allInfoUsers = function($event, element) {
-        debugger
+        // var obj = {
+        //   section: 'posts',
+        //   el: 'userId',
+        //   el_id: element,
+        // }
         $scope.userID = element;
         $scope.swowList = false;
     }
+    $scope.posts = function($event, element) {
+        // objUrl = {
+        //   section: 'posts',
+        //   el: 'userId',
+        //   el_id: element,
+        // }
+
+        // $scope.jsonData = func(objUrl);
+
+        // $scope.swowList = false;
+    }
+
+   function func(objUrl) {
+      $http({method : "GET",
+            url : 'http://jsonplaceholder.typicode.com/'+ objUrl.section+ "?" + objUrl.el + "=" + objUrl.el_id
+      })
+      .then(function (response) {
+        return response.data;
+       });
+    }
+
+
 });
 
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/user');
+    $stateProvider
+
+        .state('user', {
+            url: '/user',
+            templateUrl: 'templates/user.html'
+        })
+        
+        .state('users', {
+            url: '/users',
+            templateUrl: 'templates/allInfoUser.html'      
+        })
+
+        .state('post', {
+            url: '/post',
+            templateUrl: 'templates/post.html'      
+        })
+
+        .state('album', {
+            url: '/album',
+            templateUrl: 'templates/album.html'      
+        })
+
+        .state('photos', {
+            url: '/album/photos',
+            templateUrl: 'templates/photos.html'      
+        })
+
+        .state('comments', {
+            url: '/post/comments',
+            templateUrl: 'templates/comments.html'      
+        })
+        
+});
+
+/*
 app.directive("usersInfo", function () {
     return {
       link: function (scope, element, attrs) {
@@ -57,6 +116,8 @@ app.directive("allUsersInfo", function () {
         
       },
       restrict: 'A',
-      templateUrl:  "templates/allInfoUser.html",
+      templateUrl:  "templates/post.html",
     }
 })
+
+*/
