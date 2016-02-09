d54bb157a1da122373dc845dbdc9997520c3de3b
Andrey Cherkashin <tosmsandreyka@gmail.com>
2016-02-09 18:47:08 +0200

    repack

--- controllers/postsCtrl.js
+++ controllers/postsCtrl.js
@@ -9,6 +9,8 @@
 		$scope.userName = data.userName;
 
 		$scope.addpost = function(post) {
+
+			post.id = _.uniqueId('id:');
 			post.userId = data.data[0].userId;
 			postServices.PostRequest(post, $scope);
 		}
